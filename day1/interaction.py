09539a6d67ddad0db1b4780151b87a0d479c69d2
hekai <kai.he02@ele.me>
2018-03-18 11:25:29 +0800

    string format

--- day1/interaction.py
+++ day1/interaction.py
@@ -6,3 +6,25 @@
 #
 # Author:      hekai
 #-------------------------------------------------------------------------------
+
+name = input("name:")
+pwd = input("pwd:")
+age = int(input("age:"))
+print(type(age))
+print(name, pwd)
+
+info = '''
+age:%d
+''' %(age)
+print(info)
+
+
+info = '''
+age:{age}
+'''.format(age = age)
+print(info)
+
+info = '''
+age:{0}
+'''.format(age)
+print(info)
