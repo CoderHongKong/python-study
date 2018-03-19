4726a06ebf6b0a64f1f18549c10c554b28a132fb
hekai <kai.he02@ele.me>
2018-03-19 19:25:55 +0800

    decode&encode

--- day1/sys_module.py
+++ day1/sys_module.py
@@ -14,5 +14,13 @@ print(sys.path)
 
 cmd_res = os.popen("dir").read()
 print("-->", cmd_res)
-os.mkdir("new_dir")
+# os.mkdir("new_dir")
 
+
+msg = "管你毛事"
+
+print(msg)
+
+print(msg.encode(encoding="utf-8"))
+
+print(msg.encode(encoding="utf-8").decode(encoding="utf-8"))
